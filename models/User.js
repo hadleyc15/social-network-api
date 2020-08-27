1efebb58fde9b2cb848747b8a5278f57f550d796
Chris Hadley <hadleyc15@yahoo.com>
2020-08-27 13:43:57 -0600

    added user and thought models based off acceptance criteria

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const UserSchema = new Schema({
+    username: { 
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^([a-z0-9_\.-]+)@([a-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address!']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+}
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+UserSchema.virtual('thoughtCount').get(function() {
+    return this.thoughts.reduce((total, thought) => total + thought.reactions.length + 1, 0);
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,4 +20,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 mongoose.set('debug', true);
 
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
